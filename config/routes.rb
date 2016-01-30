cd6b1444aff350463934356117c1b8960ae9b6f1
Aman8050 <aman8050@gmail.com>
2016-01-30 12:02:56 +0530

    Add Posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Blog::Application.routes.draw do
-  resources :post
-  root 'post#index'
-  #match ':controller(/:action(/:id))',:via => :get
+  resources :posts
+  root 'posts#index'
+  match ':controller(/:action(/:id))',:via => :get
   
 end
